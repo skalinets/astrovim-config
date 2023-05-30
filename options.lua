8431cc6e2c9e8b0002604cbda48008ebcc8bbf77
Serhii Kalinets <kalinets@gmail.com>
2023-05-30 19:57:10 +0300

    chore: Update user config

--- options.lua
+++ options.lua
@@ -17,6 +17,11 @@ return {
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
   },
+ --   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+ -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+ -- ▏ Link("LineNr", "Blue")
+ -- ▏ Link("CursorLineNr", "White")
+
 }
 -- If you need more control, you can use the function()...end notation
 -- return function(local_vim)
